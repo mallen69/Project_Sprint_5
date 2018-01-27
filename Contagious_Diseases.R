76f3be7965957d7fa299d0c94fff0f7d21574d06
mallen69 <michael.nj.allen@gmail.com>
2018-01-26 14:34:38 -1000

    Upating my R file which is Contagious Diseseas and start of Power Point

--- Contagious_Diseases.R
+++ Contagious_Diseases.R
@@ -1,3 +1,24 @@
+#The start of working with the US Contagious Diseases
+
+#Setting my working Directory in order to be in the folder where the US_Contagious_Diseases is.
+setwd("C:/Users/micha/Desktop/DevLeague Begins Nov 7 2017/Project_Sprint_5/")
+
+#Loading libraries and reading the CSV file "us_contagious_diseases into R.
+
+#Loading the library dplyr in order for the 
+library(dplyr)
+
+#Loading the library ggplot2
+library(ggplot2)
+
+#Loading the library plyr in order to use the ddply() function
+library("plyr")
+
+
+#Creating the variable "sick" to represent the ""us_contagious_diseases.csv"" 
+#which was pulled from the Internet
+sick <- read.csv("us_contagious_diseases.csv")
+
 #calculating the percentage of people that are sick for that particular disease per the population
 sick <- transform(sick, percentage_sick = count / population)
 
@@ -11,17 +32,21 @@ highest_year <- max(sick$year)
 #If I wanted to know the highest counts of diseases in the dataset
 highest_count <- max(sick$count) 
 
-#If I wanted to know the highest percentage_sick that is found in the data set.  Also needed to do the na.rm=True 
-#in order for it to work.
+#If I wanted to know the highest percentage_sick that is found in the data set.  
+#Also needed to do the na.rm=True in order for it to work
 
+highest_percentage_sick <- max(sick$percentage_sick, na.rm = TRUE)
                                     
-                                    
+#Filter out all of the NA's for the percentage_sick                                    
 filter(sick, !is.na(percentage_sick))
                                     
+#Create a new variable called "Sicker" that only has the percentage_sick values without any NA's
 Sicker <- filter(sick, !is.na(percentage_sick))
                                    
- #In order to give me the highest percentage_sick for each state, still need to now figure                              #out how to get the rest of the columns back
-
-                                    
+                             
+#Calculate the top disease per State (by the highest percentage_sick)                                    
 States_highest_sick <- ddply(Sicker, 'state', function(x) x[x$percentage_sick==max(x$percentage_sick),])
-                                    View(States_highest_sick)
+
+#View the top disease per State and show the percentage_sick
+View(States_highest_sick)
+    
